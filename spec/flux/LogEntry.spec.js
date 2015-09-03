0984f1f1427050ea0d4c8ad2336396437bb9522f
Adam Phillips <aphillips@scholastic.co.uk>
2015-09-04 00:14:55 +0100

    Updated test message

--- spec/flux/LogEntry.spec.js
+++ spec/flux/LogEntry.spec.js
@@ -1,7 +1,7 @@
 var LogEntry = require('../../scripts/flux/LogEntry');
 
 describe('LogEntry', function () {
-  it('should exist', function() {
+  it('should have a type and data', function() {
     var l = new LogEntry('some type', 'some data')
 
     expect(l.type).toEqual('some type');
